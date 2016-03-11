28cf49a3fca7428f99ab65da3be8d5be67e185dd
comaw@i.ua <comaw@i.ua>
2016-03-11 16:04:06 +0200

    front user login form

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -10,25 +10,30 @@ use yii\bootstrap\ActiveForm;
 $this->title = Yii::t('app', 'Вход');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="products">
+    <h1 class="page-title"><?=Yii::t('app', 'Вход')?></h1>
     <p style="color: green;"><?=Yii::$app->session->getFlash('success')?></p>
     <p><?=Yii::t('app', 'Заполните следующие поля для входа')?>:</p>
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('app', 'Войти'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
+    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+    <table width="100%" border="0" cellspacing="4" cellpadding="4">
+        <tr>
+            <td>E-mail <b class="toRec">*</b></td>
+            <td><?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('') ?></td>
+        </tr>
+        <tr>
+            <td>Пароль <b class="toRec">*</b></td>
+            <td><?= $form->field($model, 'password')->passwordInput()->label('') ?></td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td><?= $form->field($model, 'rememberMe')->checkbox() ?></td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td>
+                <?= Html::submitButton(Yii::t('app', 'Войти'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </td>
+        </tr>
+    </table>
+    <?php ActiveForm::end(); ?>
 </div>
